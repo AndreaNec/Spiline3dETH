c909046d2b64a7b8bf12574c06ba23759a4a1da6
AndreaNec <100475101+AndreaNec@users.noreply.github.com>
2022-05-19 15:55:54 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/zAzs5Ogk4l7KnZnw/scene.splinecode');
